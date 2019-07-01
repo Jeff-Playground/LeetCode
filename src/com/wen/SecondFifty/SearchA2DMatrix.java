293704ef46fb17dca2a4cbbfb7f5ee98e290fd59
wen <wenjianxiong91@gmail.com>
2019-07-01 11:57:16 -0400

    Updates.

--- src/com/wen/SecondFifty/SearchA2DMatrix.java
+++ src/com/wen/SecondFifty/SearchA2DMatrix.java
@@ -1,6 +1,7 @@
 package com.wen.SecondFifty;
 
 public class SearchA2DMatrix {
+    // Double binary search
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix==null || matrix.length==0 || matrix[0].length==0){
             return false;
@@ -28,4 +29,28 @@ public class SearchA2DMatrix {
             return false;
         }
     }
+
+//    // Single binary search
+//    public boolean searchMatrix(int[][] matrix, int target) {
+//        if(matrix==null || matrix.length==0 || matrix[0].length==0){
+//            return false;
+//        }
+//        int m=matrix.length, n=matrix[0].length;
+//        if(target<matrix[0][0] || target>matrix[m-1][n-1]){
+//            return false;
+//        }
+//        int left=0, right=(m-1)*n+n-1;
+//        while(left<=right){
+//            int mid=left+(right-left)/2;
+//            int x=mid/n, y=mid%n;
+//            if(matrix[x][y]==target){
+//                return true;
+//            } else if(matrix[x][y]>target){
+//                right=mid-1;
+//            } else{
+//                left=mid+1;
+//            }
+//        }
+//        return false;
+//    }
 }
--- src/com/wen/SecondFifty/SearchInRotatedSortedArrayII.java
+++ src/com/wen/SecondFifty/SearchInRotatedSortedArrayII.java
@@ -1,29 +1,62 @@
 package com.wen.SecondFifty;
 
 public class SearchInRotatedSortedArrayII {
+    // return nums[right]==target because while(left<right)
     public boolean search(int[] nums, int target) {
         if(nums==null || nums.length==0){
             return false;
         }
-        int left=0, right=nums.length-1;
-        while(left<=right){
+        int l=nums.length;
+        int left=0, right=l-1;
+        while(left<right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return true;
-            } else if(nums[mid]<=nums[right]){
-                if(nums[mid]<target && nums[right]>=target){
-                    left=mid+1;
-                } else{
+            } else if(nums[left]==nums[mid]){
+                left++;
+            } else if(nums[left]<nums[mid]){
+                if(target>=nums[left] && target<nums[mid]){
                     right=mid-1;
+                } else {
+                    left=mid+1;
                 }
             } else{
-                if(nums[mid]>target && nums[left]<=target){
-                    right=mid-1;
-                } else{
+                if(target>nums[mid] && target<=nums[right]){
                     left=mid+1;
+                } else {
+                    right=mid-1;
                 }
             }
         }
-        return false;
+        return nums[right]==target;
     }
+
+//    // return false becuase while(left<=right), so impossible to have nums[right]==target or nums[left]==target in the end
+//    public boolean search(int[] nums, int target) {
+//        if(nums==null || nums.length==0){
+//            return false;
+//        }
+//        int left=0, right=nums.length-1;
+//        while(left<=right){
+//            int mid=left+(right-left)/2;
+//            if(nums[mid]==target){
+//                return true;
+//            } else if(nums[mid]<nums[right]){
+//                if(nums[mid]<target && nums[right]>=target){
+//                    left=mid+1;
+//                } else{
+//                    right=mid-1;
+//                }
+//            } else if(nums[mid]>nums[right]){
+//                if(nums[mid]>target && nums[left]<=target){
+//                    right=mid-1;
+//                } else{
+//                    left=mid+1;
+//                }
+//            } else{
+//                right--;
+//            }
+//        }
+//        return false;
+//    }
 }
