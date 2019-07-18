a1743765abc3f2d7d33efb548cbc1cc9615484dc
Jeff-Playground <46867995+Jeff-Playground@users.noreply.github.com>
2019-07-18 16:39:20 -0400

    Update MaximalSquare.java

--- src/com/wen/FifthFifty/MaximalSquare.java
+++ src/com/wen/FifthFifty/MaximalSquare.java
@@ -11,10 +11,12 @@ public class MaximalSquare {
         int result=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i==0 || j==0){
-                    dp[i][j]=matrix[i][j]=='1'?1:0;
-                } else if(matrix[i][j]=='1'){
-                    dp[i][j]=Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]))+1;
+                if(matrix[i][j]=='1'){
+                    if(i==0 || j==0){
+                        dp[i][j]=1;
+                    } else{
+                        dp[i][j]=Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]))+1;
+                    }
                 }
                 result=Math.max(result, dp[i][j]*dp[i][j]);
             }
