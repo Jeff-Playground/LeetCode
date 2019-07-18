9204a8b28581a68eb5017668930f3085de737c07
Jeff-Playground <46867995+Jeff-Playground@users.noreply.github.com>
2019-07-18 13:44:03 -0400

    Update FindMinimumInRotatedSortedArray.java

--- src/com/wen/FourthFifty/FindMinimumInRotatedSortedArray.java
+++ src/com/wen/FourthFifty/FindMinimumInRotatedSortedArray.java
@@ -1,6 +1,23 @@
 package com.wen.FourthFifty;
 
 public class FindMinimumInRotatedSortedArray {
+//    // Handle array unrotated in the loop
+//    public int findMin(int[] nums) {
+//        int left=0, right=nums.length-1;
+//        while(left<right){
+//        	if(nums[left]<=nums[right]) {
+//        		return nums[left];
+//        	}
+//        	int mid=left+(right-left)/2;
+//        	if(nums[left]<=nums[mid]) {
+//        		left=mid+1;
+//        	} else {
+//        		right=mid;
+//        	}
+//        }
+//        return nums[right];
+//    }
+    
     public static int findMin(int[] nums) {
         int left=0, right=nums.length-1;
         if(nums[left]<nums[right]){
