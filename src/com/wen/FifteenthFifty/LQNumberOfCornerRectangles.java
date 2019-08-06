7969484bc879c9af19b447b930c456f4ea69e76b
wen <wenjianxiong91@gmail.com>
2019-08-06 13:48:50 -0400

    Updates.

---
+++ src/com/wen/FifteenthFifty/LQNumberOfCornerRectangles.java
@@ -0,0 +1,32 @@
+package com.wen.FifteenthFifty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LQNumberOfCornerRectangles {
+    public int countCornerRectangles(int[][] grid) {
+        if(grid==null || grid.length==0 || grid[0].length==0){
+            return 0;
+        }
+        int m=grid.length, n=grid[0].length;
+        int result=0;
+        for(int i=0; i<m-1; i++){
+            List<Integer> ones=new ArrayList<>();
+            for(int k=0; k<n; k++){
+                if(grid[i][k]==1){
+                    ones.add(k);
+                }
+            }
+            for(int j=i+1; j<m; j++){
+                int count=0;
+                for(int k: ones){
+                    if(grid[j][k]==1){
+                        count++;
+                    }
+                }
+                result+=count*(count-1)/2;
+            }
+        }
+        return result;
+    }
+}
