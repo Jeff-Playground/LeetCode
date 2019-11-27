2b3cd7bc7ea1861ecb37fb2fb0aa974819ab5dde
wen <wenjianxiong91@gmail.com>
2019-11-26 21:26:18 -0500

    Updates.

--- src/com/wen/SecondFifty/RecoverBinarySearchTree.java
+++ src/com/wen/SecondFifty/RecoverBinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.wen.SecondFifty;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Stack;
 
 public class RecoverBinarySearchTree {
@@ -10,64 +13,53 @@ public class RecoverBinarySearchTree {
         public TreeNode(int x) { val = x; }
     }
 
-    // Stack
+//    // Stack
+//    public void recoverTree(TreeNode root) {
+//        Stack<TreeNode> stack=new Stack<>();
+//        TreeNode cur=root, pre=null, first=null, second=null;
+//        while(cur!=null || !stack.isEmpty()){
+//            if(cur!=null){
+//                stack.push(cur);
+//                cur=cur.left;
+//            } else{
+//                cur=stack.pop();
+//                if(pre!=null && pre.val>=cur.val){
+//                    if(first==null){
+//                        first=pre;
+//                    }
+//                    second=cur;
+//                }
+//                pre=cur;
+//                cur=cur.right;
+//            }
+//        }
+//        if(first!=null && second!=null){
+//            int temp=first.val;
+//            first.val=second.val;
+//            second.val=temp;
+//        }
+//    }
+
+    // This works for any nodes out of order because it sorts values of all nodes
     public void recoverTree(TreeNode root) {
-        Stack<TreeNode> stack=new Stack<>();
-        TreeNode cur=root, pre=null, first=null, second=null;
-        while(cur!=null || !stack.isEmpty()){
-            if(cur!=null){
-                stack.push(cur);
-                cur=cur.left;
-            } else{
-                cur=stack.pop();
-                if(pre!=null && pre.val>=cur.val){
-                    if(first==null){
-                        first=pre;
-                    }
-                    second=cur;
-                }
-                pre=cur;
-                cur=cur.right;
-            }
-        }
-        if(first!=null && second!=null){
-            int temp=first.val;
-            first.val=second.val;
-            second.val=temp;
+        List<TreeNode> nodes=new ArrayList<>();
+        List<Integer> values=new ArrayList<>();
+        treeTraverse(root, nodes, values);
+        values.sort((a, b)->a-b);
+        for(int i=0; i<nodes.size(); i++){
+            nodes.get(i).val=values.get(i);
         }
     }
 
-//    // This works for any nodes out of order because it sorts values of all nodes
-//    public void recoverTree(TreeNode root) {
-//        List<TreeNode> nodes=new ArrayList<>();
-//        List<Integer> values=new ArrayList<>();
-//        treeTraverse(root, nodes, values);
-//        values.sort(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer n1, Integer n2) {
-//                if(n1<n2){
-//                    return -1;
-//                } else if(n1>n2){
-//                    return 1;
-//                } else{
-//                    return 0;
-//                }
-//            }
-//        });
-//        for(int i=0; i<nodes.size(); i++){
-//            nodes.get(i).val=values.get(i);
-//        }
-//    }
-//
-//    private void treeTraverse(TreeNode root, List<TreeNode> nodes, List<Integer> values) {
-//        if(root==null){
-//            return;
-//        }
-//        treeTraverse(root.left, nodes, values);
-//        nodes.add(root);
-//        values.add(root.val);
-//        treeTraverse(root.right, nodes, values);
-//    }
+    private void treeTraverse(TreeNode root, List<TreeNode> nodes, List<Integer> values) {
+        if(root==null){
+            return;
+        }
+        treeTraverse(root.left, nodes, values);
+        nodes.add(root);
+        values.add(root.val);
+        treeTraverse(root.right, nodes, values);
+    }
 
 //    // Morris traversal
 //    public void recoverTree(TreeNode root) {
