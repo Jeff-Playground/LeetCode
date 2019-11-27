66f3f821b2dfc43886843926bba959ce5b5cb386
wen <wenjianxiong91@gmail.com>
2019-11-27 10:51:15 -0500

    Updates.

---
+++ src/com/wen/SeventhFifty/MinimumHeightTrees.java
@@ -0,0 +1,49 @@
+package com.wen.SeventhFifty;
+
+import java.util.*;
+
+public class MinimumHeightTrees {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n < 3) {
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                result.add(i);
+            }
+            return result;
+        }
+        Map<Integer, Set<Integer>> tree = new HashMap<>();
+        for (int[] edge : edges) {
+            Set<Integer> s0 = tree.getOrDefault(edge[0], new HashSet<>());
+            s0.add(edge[1]);
+            tree.put(edge[0], s0);
+            Set<Integer> s1 = tree.getOrDefault(edge[1], new HashSet<>());
+            s1.add(edge[0]);
+            tree.put(edge[1], s1);
+        }
+        Set<Integer> nodes = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            nodes.add(i);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int node : tree.keySet()) {
+            if (tree.get(node).size() == 1) {
+                q.offer(node);
+                nodes.remove(node);
+            }
+        }
+        while (nodes.size() > 2 && !q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                for (int nghbr : tree.get(cur)) {
+                    tree.get(nghbr).remove(cur);
+                    if (tree.get(nghbr).size() == 1) {
+                        q.offer(nghbr);
+                        nodes.remove(nghbr);
+                    }
+                }
+            }
+        }
+        return new ArrayList(nodes);
+    }
+}
