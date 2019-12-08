b552356298917506e4dde5d08da2d39c80724aa6
wen <wenjianxiong91@gmail.com>
2019-12-08 18:54:14 -0500

    Updates.

--- src/com/wen/TenthFifty/CircularArrayLoop.java
+++ src/com/wen/TenthFifty/CircularArrayLoop.java
@@ -7,9 +7,12 @@ public class CircularArrayLoop {
         } else {
             int l = nums.length;
             for(int i=0; i<l; i++){
+                // The original array doesn't have 0s, 0s are marked visited positions
                 if(nums[i]==0){
                     continue;
                 }
+                // Note the start position for slow and fast are different, this is to ensure no false positive for the
+                // first if in the while loop
                 int slow=i, fast=getNext(nums, i), start=nums[i];
                 while(start*nums[fast]>0 && start*nums[getNext(nums, fast)]>0){
                     if(fast==slow){
@@ -23,6 +26,7 @@ public class CircularArrayLoop {
                     fast=getNext(nums, getNext(nums, fast));
                 }
                 slow=i;
+                // Because the given array doesn't have 0s, mark the visited elements to be 0
                 while(start*nums[slow]>0){
                     int temp=getNext(nums, slow);
                     nums[slow]=0;
