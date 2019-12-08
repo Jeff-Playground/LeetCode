44fc6d1b27e0379771e3bd3fbd85a56835330784
wen <wenjianxiong91@gmail.com>
2019-12-08 15:17:13 -0500

    Updates.

--- src/com/wen/FifthFifty/MinimumSizeSubarraySum.java
+++ src/com/wen/FifthFifty/MinimumSizeSubarraySum.java
@@ -34,9 +34,9 @@ public class MinimumSizeSubarraySum {
 //        for(int i=1; i<nums.length+1; i++){
 //            sums[i]=sums[i-1]+nums[i-1];
 //        }
-//        for(int i=0; i<nums.length; i++){
+//        for(int i=0; i<sums.length; i++){
 //            int right=searchRight(i+1, s+sums[i], sums);
-//            if(right>nums.length){
+//            if(right>sums.length){
 //                break;
 //            }
 //            result=Math.min(result, right-i);
