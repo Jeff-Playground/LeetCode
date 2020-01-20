01e05e5b7471a340b4fce89ca62d0e46a5d607c0
wen <wenjianxiong91@gmail.com>
2020-01-20 17:31:11 -0500

    Updates.

--- src/com/wen/ThirdFifty/WordBreak.java
+++ src/com/wen/ThirdFifty/WordBreak.java
@@ -3,28 +3,28 @@ package com.wen.ThirdFifty;
 import java.util.*;
 
 public class WordBreak {
-    // BFS
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict=new HashSet<>(wordDict);
-        boolean[] visited=new boolean[s.length()];
-        Queue<Integer> queue=new LinkedList<>();
-        queue.offer(0);
-        while(!queue.isEmpty()){
-            int start=queue.poll();
-            if(!visited[start]){
-                for(int i=start+1; i<=s.length(); i++){
-                    if(dict.contains(s.substring(start, i))){
-                        queue.offer(i);
-                        if(i==s.length()){
-                            return true;
-                        }
-                    }
-                }
-                visited[start]=true;
-            }
-        }
-        return false;
-    }
+//    // BFS
+//    public boolean wordBreak(String s, List<String> wordDict) {
+//        Set<String> dict=new HashSet<>(wordDict);
+//        boolean[] visited=new boolean[s.length()];
+//        Queue<Integer> queue=new LinkedList<>();
+//        queue.offer(0);
+//        while(!queue.isEmpty()){
+//            int start=queue.poll();
+//            if(!visited[start]){
+//                for(int i=start+1; i<=s.length(); i++){
+//                    if(dict.contains(s.substring(start, i))){
+//                        queue.offer(i);
+//                        if(i==s.length()){
+//                            return true;
+//                        }
+//                    }
+//                }
+//                visited[start]=true;
+//            }
+//        }
+//        return false;
+//    }
 
 //    // Dynamic Programming
 //    public boolean wordBreak(String s, List<String> wordDict) {
@@ -42,7 +42,7 @@ public class WordBreak {
 //        return dp[s.length()];
 //    }
 
-//    // DFS
+//    // DFS, memo values 0 means not visited, 1 means matching, -1 means not matching
 //    public boolean wordBreak(String s, List<String> wordDict) {
 //        Set<String> dict=new HashSet<>(wordDict);
 //        int[] memo=new int[s.length()];
@@ -66,26 +66,29 @@ public class WordBreak {
 //        return false;
 //    }
 
-//    // My way
-//    public boolean wordBreak(String s, List<String> wordDict) {
-//        if(s.isEmpty()) {
-//            return true;
-//        }
-//        for(String word: wordDict) {
-//            if(s.contains(word)) {
-//                String[] parts=s.split(word);
-//                if(parts.length==0) {
-//                    return true;
-//                }
-//                boolean result=wordBreak(parts[0], wordDict);
-//                for(int i=1; i<parts.length; i++) {
-//                    result=result && wordBreak(parts[i], wordDict);
-//                }
-//                if(result) {
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
+    // Works better when wordDict size is relatively small
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(s.isEmpty()) {
+            return true;
+        }
+        for(String word: wordDict) {
+            if(s.contains(word)) {
+                String[] parts=s.split(word);
+                if(parts.length==0) {
+                    return true;
+                }
+                boolean result=wordBreak(parts[0], wordDict);
+                for(int i=1; i<parts.length; i++) {
+                    if(!result){
+                        break;
+                    }
+                    result=result && wordBreak(parts[i], wordDict);
+                }
+                if(result) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
