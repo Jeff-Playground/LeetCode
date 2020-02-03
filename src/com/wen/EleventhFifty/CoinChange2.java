341ac8583a7b9b308ec8fd767ecbba7c3b98cf16
wen <wenjianxiong91@gmail.com>
2020-02-03 11:45:42 -0500

    Updates.

--- src/com/wen/EleventhFifty/CoinChange2.java
+++ src/com/wen/EleventhFifty/CoinChange2.java
@@ -1,13 +1,15 @@
 package com.wen.EleventhFifty;
 
 public class CoinChange2 {
+    // Here using unbounded knapsack is simpler and makes more sense, the bounded knapsack solutions are just for practice
+
 //    // DP, bounded knapsack optimized to one dimensional array
 //    public static int change(int amount, int[] coins) {
 //        int l=coins.length;
 //        int[] dp=new int[amount+1];
 //        dp[0]=1;
 //        for(int i=1; i<l+1; i++){
-//            for(int j=amount; j>=1; j--){
+//            for(int j=amount; j>=0; j--){
 //                if(coins[i-1]<=j){
 //                    int max=j/coins[i-1];
 //                    // Note here k starts with 1 because dp[j] already included the value for k=0
@@ -26,11 +28,9 @@ public class CoinChange2 {
 //    public int change(int amount, int[] coins) {
 //        int l=coins.length;
 //        int[][] dp=new int[l+1][amount+1];
-//        for(int i=0; i<l+1; i++){
-//            dp[i][0]=1;
-//        }
+//        dp[0][0]=1;
 //        for(int i=1; i<l+1; i++){
-//            for(int j=1; j<amount+1; j++){
+//            for(int j=0; j<amount+1; j++){
 //                if(coins[i-1]<=j){
 //                    int max=j/coins[i-1];
 //                    // Note here k starts with 0 because dp[i][j] starts with 0
@@ -45,39 +45,37 @@ public class CoinChange2 {
 //        return dp[l][amount];
 //    }
 
-    // DP, unbounded knapsack optimized to one dimensional array
-    public int change(int amount, int[] coins) {
-        int l=coins.length;
-        int[] dp=new int[amount+1];
-        dp[0]=1;
-        for(int i=1; i<l+1; i++){
-            for(int j=1; j<amount+1; j++){
-                if(coins[i-1]<=j){
-                    dp[j]=dp[j]+dp[j-coins[i-1]];
-                } else{
-                    dp[j]=dp[j];
-                }
-            }
-        }
-        return dp[amount];
-    }
-
-//    // DP, unbounded knapsack
+//    // DP, unbounded knapsack optimized to one dimensional array
 //    public int change(int amount, int[] coins) {
 //        int l=coins.length;
-//        int[][] dp=new int[l+1][amount+1];
-//        for(int i=0; i<l+1; i++){
-//            dp[i][0]=1;
-//        }
+//        int[] dp=new int[amount+1];
+//        dp[0]=1;
 //        for(int i=1; i<l+1; i++){
-//            for(int j=1; j<amount+1; j++){
+//            for(int j=0; j<amount+1; j++){
 //                if(coins[i-1]<=j){
-//                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+//                    dp[j]=dp[j]+dp[j-coins[i-1]];
 //                } else{
-//                    dp[i][j]=dp[i-1][j];
+//                    dp[j]=dp[j];
 //                }
 //            }
 //        }
-//        return dp[l][amount];
+//        return dp[amount];
 //    }
+
+    // DP, unbounded knapsack
+    public int change(int amount, int[] coins) {
+        int l=coins.length;
+        int[][] dp=new int[l+1][amount+1];
+        dp[0][0]=1;
+        for(int i=1; i<l+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                } else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[l][amount];
+    }
 }
