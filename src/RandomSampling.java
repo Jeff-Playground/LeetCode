90399977050edd0e03c7c6ad68507613887a5163
wen <wenjianxiong91@gmail.com>
2020-02-10 20:44:41 -0500

    Updates.

--- src/RandomSampling.java
+++ src/RandomSampling.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.Set;
 
 public class RandomSampling {
+    // Note random.nextInt(n) will return a number in 0 to n-1
     public static void main(String[] args) {
         List<Integer> nums=new ArrayList<>();
         for(int i=0; i<10000; i++) {
@@ -12,7 +13,8 @@ public class RandomSampling {
         }
 //		List<Integer> result=randomSampling(10, 10000, nums);
 //		List<Integer> result=resovoirRandomSampling(10, 10000, nums);
-        List<Integer> result=selectionRandomSampling(10, 10000, nums);
+//      List<Integer> result=selectionRandomSampling(10, 10000, nums);
+        List<Integer> result=randomSamplingBasedOnKnuthShuffling(10, 10000, nums);
         result.stream().forEach(System.out::println);
     }
 
@@ -36,7 +38,7 @@ public class RandomSampling {
 
     // Reservoir sampling:
     // Pick the first m elements from the given list, then for the coming elements, swap it into the sampled m elements with a
-    // probability of (m/i+1), where i is the index of the element.
+    // probability of m/(i+1), where i is the index of the element.
     // To prove this:
     // 		For the kth element after the first m elements, the probability of it being in the final result is:
     // 			m/k		*		(1-m/(k+1)+m/(k+1)*(m-1)/m)		*...*	(1-m/n+m/n*(m-1)/m)		=	m/n
@@ -79,6 +81,25 @@ public class RandomSampling {
                 result.add(nums.get(i));
                 sampled++;
             }
+        }
+        return result;
+    }
+
+    // Knuth shuffling:
+    private static List<Integer> randomSamplingBasedOnKnuthShuffling(int m, int n, List<Integer> nums){
+        List<Integer> result=new ArrayList<>();
+        Random r=new Random();
+        int l=nums.size();
+        for(int i=0; i<l; i++){
+            int index=r.nextInt(l-i)+i;
+            if(index!=i) {
+                int temp = nums.get(i);
+                nums.set(i, nums.get(index));
+                nums.set(index, temp);
+            }
+        }
+        for(int i=0; i<m; i++){
+            result.add(nums.get(i));
         }
         return result;
     }
--- src/com/wen/EighthFifty/ShuffleAnArray.java
+++ src/com/wen/EighthFifty/ShuffleAnArray.java
@@ -6,9 +6,9 @@ import java.util.Random;
 public class ShuffleAnArray {
     // To prove the shuffle is really random(each element can appear at each position with same possibility, and in the
     // final result each position can have each element with same possibility):
-    // For the 1st position, each element appear at possibility: 1/n
+    // For the 1st position, each element appear at possibility: n/n * 1/n = 1/n
     // For the 2nd position, the remaining (n-1) elements appear at possibility: (n-1)/n * 1/(n-1) = 1/n
-    // For the 3rd position, the remaining (n-2) elements appear at possibility: (n-1)/n * (n-2)/(n-1) * 1/(n-2) = 1/n
+    // For the 3rd position, the remaining (n-2) elements appear at possibility: (n-2)/n * 1/(n-2) = 1/n
     // ...
     // So in the final result each position can have each element with same possibility
     class Solution {
