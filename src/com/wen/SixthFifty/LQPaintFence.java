50a90b5745cd6a53ce59e16173ff0858bfda14e8
wen <wenjianxiong91@gmail.com>
2020-02-24 22:23:37 -0500

    Updates.

--- src/com/wen/SixthFifty/LQPaintFence.java
+++ src/com/wen/SixthFifty/LQPaintFence.java
@@ -15,6 +15,8 @@ public class LQPaintFence {
     }
 
 //    // DP
+//    // dp[i][0] stands for the number of ways to paint the post at i to a color different than the previous post,
+//    // dp[i][1] stands for the number of ways to paint the post at i to a color same with the previous post
 //    public int numWays(int n, int k) {
 //        int[][] dp=new int[n][2];
 //        for(int i=0; i<n; i++){
@@ -22,7 +24,11 @@ public class LQPaintFence {
 //                dp[i][0]=k;
 //                dp[i][1]=0;
 //            } else{
+//                // For a different color, it's the total number of previous ways multiply by (k-1) because current color
+//                // can be whichever color except the previous ONE
 //                dp[i][0]=(dp[i-1][0]+dp[i-1][1])*(k-1);
+//                // For a same color, the previous color can't be the same with the color before it, and it's the same
+//                // ONE color
 //                dp[i][1]=dp[i-1][0];
 //            }
 //        }
--- src/com/wen/SixthFifty/UglyNumberII.java
+++ src/com/wen/SixthFifty/UglyNumberII.java
@@ -1,26 +1,28 @@
 package com.wen.SixthFifty;
 
 public class UglyNumberII {
+    // Get the sequence by multiply the existing ugly numbers to get the new ones
+    // The index array stores the next index of the ugly number which will be multiplied by the corresponding factor
     public int nthUglyNumber(int n) {
         int[] factors={2,3,5};
-        int fn=factors.length;
-        int[] indexes=new int[fn];
+        int fl=factors.length;
+        int[] index=new int[fl];
         int[] ugly=new int[n];
         ugly[0]=1;
-        int count=1;
-        while(count<n){
-            int min=Integer.MAX_VALUE, index=-1;
-            for(int i=0; i<fn; i++){
-                if(min>factors[i]*ugly[indexes[i]]){
-                    min=factors[i]*ugly[indexes[i]];
-                    index=i;
+        int cur=1;
+        while(cur<n){
+            int min=Integer.MAX_VALUE, fIndex=-1;
+            for(int i=0; i<fl; i++){
+                if(min>factors[i]*ugly[index[i]]){
+                    min=factors[i]*ugly[index[i]];
+                    fIndex=i;
                 }
             }
-            if(min!=ugly[count-1]){
-                ugly[count]=min;
-                count++;
+            if(min!=ugly[cur-1]){
+                ugly[cur]=min;
+                cur++;
             }
-            indexes[index]++;
+            index[fIndex]++;
         }
         return ugly[n-1];
     }
