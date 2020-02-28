e23a35d82126c3900e9ffd393af6a3673830b8d4
wen <wenjianxiong91@gmail.com>
2020-02-27 22:07:26 -0500

    Updates.

--- src/com/wen/FifthFifty/ContainsDuplicateIII.java
+++ src/com/wen/FifthFifty/ContainsDuplicateIII.java
@@ -1,13 +1,38 @@
 package com.wen.FifthFifty;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class ContainsDuplicateIII {
-    // Note convert int to long to avoid overflow
+//    // Note convert int to long to avoid overflow
+//    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+//        for(int i=0; i<nums.length; i++){
+//            for(int j=i+1; j<nums.length && j-i<k+1; j++){
+//                if(Math.abs((long)nums[i]-(long)nums[j])<=t){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length && j-i<k+1; j++){
-                if(Math.abs((long)nums[i]-(long)nums[j])<=t){
-                    return true;
-                }
+        if(nums==null || nums.length<2){
+            return false;
+        }
+        int l=nums.length;
+        TreeSet<Integer> window=new TreeSet<>();
+        for(int i=0; i<l; i++){
+            Integer low=window.floor(nums[i]);
+            Integer high=window.ceiling(nums[i]);
+            if((low!=null && (long)nums[i]-low<=t) || (high!=null && (long)high-nums[i]<=t)){
+                return true;
+            }
+            window.add(nums[i]);
+            // Note this won't remove duplicates from the window and cause problem because if there's duplicates, it
+            // should already been identified as true and won't get to this step
+            if(i>=k){
+                window.remove(nums[i-k]);
             }
         }
         return false;
