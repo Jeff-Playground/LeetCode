692f1f1b35a85827a2522e0806d425a226f42ce6
wen <wenjianxiong91@gmail.com>
2020-04-29 15:42:48 -0400

    Revert: Updates.

--- src/com/wen/SeventhFifty/SuperUglyNumber.java
+++ src/com/wen/SeventhFifty/SuperUglyNumber.java
@@ -1,20 +1,17 @@
 package com.wen.SeventhFifty;
 
-import java.util.HashSet;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class SuperUglyNumber {
     public int nthSuperUglyNumber(int n, int[] primes) {
         int k=primes.length;
         PriorityQueue<Integer> pq=new PriorityQueue<>((a, b)->a-b);
         pq.offer(1);
-        Set<Integer> visited=new HashSet<>();
-        int count=0, cur=-1;
+        int count=0, cur=-1, last=-1;
         while(count<n){
             cur=pq.poll();
-            if(!visited.contains(cur)){
-                visited.add(cur);
+            if(cur!=last){
+                last=cur;
                 count++;
                 for(int p:primes){
                     if((long)cur*p<=Integer.MAX_VALUE){
