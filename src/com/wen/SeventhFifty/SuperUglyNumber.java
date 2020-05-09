b66acdd384ea5c3291f6daef92eefff4261ca33c
wen <wenjianxiong91@gmail.com>
2020-05-09 16:58:51 -0400

    Revert: Updates.

--- src/com/wen/SeventhFifty/SuperUglyNumber.java
+++ src/com/wen/SeventhFifty/SuperUglyNumber.java
@@ -4,22 +4,43 @@ import java.util.PriorityQueue;
 
 public class SuperUglyNumber {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int k=primes.length;
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a, b)->a-b);
-        pq.offer(1);
-        int count=0, cur=-1, last=-1;
-        while(count<n){
-            cur=pq.poll();
-            if(cur!=last){
-                last=cur;
-                count++;
-                for(int p:primes){
-                    if((long)cur*p<=Integer.MAX_VALUE){
-                        pq.offer(cur*p);
-                    }
+        int l=primes.length, cur=1;
+        int[] idx=new int[l], ugly=new int[n];
+        ugly[0]=1;
+        while(cur<n){
+            int min=Integer.MAX_VALUE, index=-1;
+            for(int i=0; i<l; i++){
+                if(primes[i]*ugly[idx[i]]<min){
+                    min=primes[i]*ugly[idx[i]];
+                    index=i;
                 }
             }
+            if(min!=ugly[cur-1]){
+                ugly[cur++]=min;
+            }
+            idx[index]++;
         }
-        return cur;
+        return ugly[n-1];
     }
+
+//    // Heap
+//    public int nthSuperUglyNumber(int n, int[] primes) {
+//        int k=primes.length;
+//        PriorityQueue<Integer> pq=new PriorityQueue<>((a, b)->a-b);
+//        pq.offer(1);
+//        int count=0, cur=-1, last=-1;
+//        while(count<n){
+//            cur=pq.poll();
+//            if(cur!=last){
+//                last=cur;
+//                count++;
+//                for(int p:primes){
+//                    if((long)cur*p<=Integer.MAX_VALUE){
+//                        pq.offer(cur*p);
+//                    }
+//                }
+//            }
+//        }
+//        return cur;
+//    }
 }
--- src/com/wen/SixthFifty/UglyNumberII.java
+++ src/com/wen/SixthFifty/UglyNumberII.java
@@ -1,29 +1,55 @@
 package com.wen.SixthFifty;
 
+import java.util.PriorityQueue;
+
 public class UglyNumberII {
-    // Get the sequence by multiply the existing ugly numbers to get the new ones
-    // The index array stores the next index of the ugly number which will be multiplied by the corresponding factor
+//    // Get the sequence by multiply the existing ugly numbers to get the new ones
+//    // The index array stores the next index of the ugly number which will be multiplied by the corresponding factor
+//    public int nthUglyNumber(int n) {
+//        int[] factors={2,3,5};
+//        int fl=factors.length;
+//        int[] index=new int[fl];
+//        int[] ugly=new int[n];
+//        ugly[0]=1;
+//        int cur=1;
+//        while(cur<n){
+//            int min=Integer.MAX_VALUE, fIndex=-1;
+//            for(int i=0; i<fl; i++){
+//                if(min>factors[i]*ugly[index[i]]){
+//                    min=factors[i]*ugly[index[i]];
+//                    fIndex=i;
+//                }
+//            }
+//            if(min!=ugly[cur-1]){
+//                ugly[cur]=min;
+//                cur++;
+//            }
+//            index[fIndex]++;
+//        }
+//        return ugly[n-1];
+//    }
+
+    // Heap
     public int nthUglyNumber(int n) {
-        int[] factors={2,3,5};
-        int fl=factors.length;
-        int[] index=new int[fl];
-        int[] ugly=new int[n];
-        ugly[0]=1;
-        int cur=1;
-        while(cur<n){
-            int min=Integer.MAX_VALUE, fIndex=-1;
-            for(int i=0; i<fl; i++){
-                if(min>factors[i]*ugly[index[i]]){
-                    min=factors[i]*ugly[index[i]];
-                    fIndex=i;
-                }
+        int result=-1, count=0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a, b)->a-b);
+        pq.offer(1);
+        while(count++<n){
+            int cur=pq.poll();
+            while(cur==result){
+                cur=pq.poll();
             }
-            if(min!=ugly[cur-1]){
-                ugly[cur]=min;
-                cur++;
+            if(cur*2l<=Integer.MAX_VALUE){
+                pq.offer(cur*2);
             }
-            index[fIndex]++;
+            if(cur*3l<=Integer.MAX_VALUE){
+                pq.offer(cur*3);
+            }
+            if(cur*5l<=Integer.MAX_VALUE){
+                pq.offer(cur*5);
+            }
+            result=cur;
         }
-        return ugly[n-1];
+        return result;
     }
 }
