5d0d386f4494a665e39c6595d6cfb6cc0e874eb1
wen <wenjianxiong91@gmail.com>
2020-10-13 20:29:49 -0400

    Updates.

---
+++ src/com/wen/TwelfthFifty/LQ Find Median Given Frequency of Numbers.sql
@@ -0,0 +1,28 @@
+/*
+
+The Numbers table keeps the value of number and its frequency.
+
++----------+-------------+
+|  Number  |  Frequency  |
++----------+-------------|
+|  0       |  7          |
+|  1       |  1          |
+|  2       |  3          |
+|  3       |  1          |
++----------+-------------+
+
+In this table, the numbers are 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 3, so the median is (0 + 0) / 2 = 0.
+
++--------+
+| median |
++--------|
+| 0.0000 |
++--------+
+Write a query to find the median of all numbers and name the result as median.
+
+*/
+
+select  avg(n.Number) median
+from Numbers n
+where n.Frequency >= abs((select sum(Frequency) from Numbers where Number<=n.Number) -
+                         (select sum(Frequency) from Numbers where Number>=n.Number))
