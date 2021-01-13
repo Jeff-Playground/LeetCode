c10969fbb899283c8ff6b82936b8a6bda919a076
wen <wenjianxiong91@gmail.com>
2021-01-13 18:48:42 -0500

    Updates.

--- src/com/wen/ThirteenthFifty/LQDesignCompressedStringIterator.java
+++ src/com/wen/ThirteenthFifty/LQDesignCompressedStringIterator.java
@@ -31,32 +31,124 @@ iterator.next(); // return ' '
 
  */
 
-public class LQDesignCompressedStringIterator {
-    class StringIterator{
-        public int idx=-1;
-        public int length=-1;
-        public String value="";
+import java.util.LinkedList;
+import java.util.Queue;
 
-        public StringIterator(String value){
-            this.value=value;
-            this.idx=0;
-            this.length=value.length();
+public class LQDesignCompressedStringIterator {
+    // Use a Queue2
+    class  StringIterator{
+        private Queue<Character> q;
+
+        public StringIterator(String compressedString){
+            int idx=0;
+            q=new LinkedList<>();
+            while(idx<compressedString.length()){
+                char c=compressedString.charAt(idx++);
+                int count=0;
+                while(compressedString.charAt(idx)>='0' && compressedString.charAt(idx)<='9'){
+                    count=count*10+compressedString.charAt(idx++)-'0';
+                }
+                while(count-->0){
+                    q.offer(c);
+                }
+            }
         }
 
         public char next(){
             if(hasNext()){
-                return value.charAt(idx++);
+                return q.poll();
             } else{
                 return ' ';
             }
         }
 
         public boolean hasNext(){
-            if(length>0 && idx<length){
-                return true;
-            } else{
-                return false;
-            }
+            return !q.isEmpty();
         }
     }
+
+//    // Use a Queue
+//    class  StringIterator{
+//        private Queue<String> q;
+//        char c;
+//        int count=-1;
+//
+//        public StringIterator(String compressedString){
+//            int idx=0;
+//            q=new LinkedList<>();
+//            while(idx<compressedString.length()){
+//                q.offer(compressedString.charAt(idx++)+"");
+//                StringBuilder count=new StringBuilder();
+//                while(compressedString.charAt(idx)>='0' && compressedString.charAt(idx)<='9'){
+//                    count.append(compressedString.charAt(idx++));
+//                }
+//                q.offer(count.toString());
+//            }
+//        }
+//
+//        public char next(){
+//            if(hasNext()){
+//                count--;
+//                return c;
+//            } else{
+//                return ' ';
+//            }
+//        }
+//
+//        public boolean hasNext(){
+//            if(count>0){
+//                return true;
+//            } else{
+//                if(q.isEmpty()){
+//                    return false;
+//                } else{
+//                    c=q.poll().charAt(0);
+//                    count=Integer.valueOf(q.poll());
+//                    return true;
+//                }
+//            }
+//        }
+//    }
+
+//    // Maintain count and idx to track status
+//    class StringIterator{
+//        public int length=-1;
+//        public int idx=-1;
+//        public int count=-1;
+//        public String compressedString="";
+//        public char c=' ';
+//
+//        public StringIterator(String compressedString){
+//            this.compressedString=compressedString;
+//            this.length=compressedString.length();
+//            this.idx=0;
+//            this.count=0;
+//            this.c=' ';
+//        }
+//
+//        public char next(){
+//            if(hasNext()){
+//                count--;
+//                return c;
+//            } else{
+//                return ' ';
+//            }
+//        }
+//
+//        public boolean hasNext(){
+//            if(count>0){
+//                return true;
+//            } else{
+//                if(length>0 && idx<length){
+//                    c=compressedString.charAt(idx++);
+//                    while(compressedString.charAt(idx)>='0' && compressedString.charAt(idx)<='9'){
+//                        count=count*10+compressedString.charAt(idx++)-'0';
+//                    }
+//                    return true;
+//                } else{
+//                    return false;
+//                }
+//            }
+//        }
+//    }
 }
