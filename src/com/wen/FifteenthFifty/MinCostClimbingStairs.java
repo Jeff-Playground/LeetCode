d1d443897baf80928cb1e11c6481dac9dfc64a5f
wen <wenjianxiong91@gmail.com>
2022-03-14 20:10:22 -0400

    Updates.

--- src/com/wen/FifteenthFifty/MinCostClimbingStairs.java
+++ src/com/wen/FifteenthFifty/MinCostClimbingStairs.java
@@ -1,15 +1,33 @@
 package com.wen.FifteenthFifty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinCostClimbingStairs {
-    // DP2
+    // DP2, bottom up
     public static int minCostClimbingStairs(int[] cost) {
-        for(int i=2; i<cost.length; i++){
+        for(int i=2; i<cost.length-1; i++){
             cost[i]+=Math.min(cost[i-2], cost[i-1]);
         }
         return Math.min(cost[cost.length-2], cost[cost.length-1]);
     }
 
-//    // DP1
+//    // DP1, top down
+//    public int minCostClimbingStairs(int[] cost) {
+//        Map<Integer, Integer> cache=new HashMap<>();
+//        cache.put(0, 0);
+//        cache.put(1, 0);
+//        return mccsHelper(cost, cache, cost.length);
+//    }
+//
+//    private int mccsHelper(int[] cost, Map<Integer, Integer> cache, int i){
+//        if(!cache.containsKey(i)){
+//            cache.put(i, Math.min(mccsHelper(cost, cache, i-1)+cost[i-1], mccsHelper(cost, cache, i-2)+cost[i-2]));
+//        }
+//        return cache.get(i);
+//    }
+
+//    // DP1, bottom up
 //    // min[i] stands for the minimum cost to reach stair i
 //    public int minCostClimbingStairs(int[] cost) {
 //        int[] min=new int[cost.length+1];
--- src/com/wen/FourthFifty/HouseRobber.java
+++ src/com/wen/FourthFifty/HouseRobber.java
@@ -1,29 +1,27 @@
 package com.wen.FourthFifty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HouseRobber {
-    // DP
-    // dp[i][0] stands for the max money made not robbing house i, dp[i][1] for the max money made robbing house i
-    public static int rob(int[] nums) {
-        if(nums==null || nums.length==0){
-            return 0;
+    // DP top down
+    public int rob(int[] nums) {
+        Map<Integer, Integer> cache=new HashMap<>();
+        cache.put(0, nums[0]);
+        if(nums.length>1){
+            cache.put(1, Math.max(nums[0], nums[1]));
         }
-        int[][] dp=new int[nums.length][2];
-        for(int i=0; i<nums.length; i++){
-            if(i==0){
-                dp[i][0]=0;
-                dp[i][1]=nums[i];
-            } else if(i==1){
-                dp[i][0]=dp[i-1][1];
-                dp[i][1]=nums[i];
-            } else{
-                dp[i][0]=Math.max(dp[i-1][0], dp[i-1][1]);
-                dp[i][1]=dp[i-1][0]+nums[i];
-            }
-        }
-        return Math.max(dp[nums.length-1][0], dp[nums.length-1][1]);
+        return robHelper(nums, cache, nums.length-1);
     }
 
-//    // Optimized DP
+    private int robHelper(int[] nums, Map<Integer, Integer> cache, int i){
+        if(!cache.containsKey(i)){
+            cache.put(i, Math.max(robHelper(nums, cache, i-2)+nums[i], robHelper(nums, cache, i-1)));
+        }
+        return cache.get(i);
+    }
+
+//    // DP bottom up, dp[i] stands for the maximum money can be got at house i
 //    public int rob(int[] nums) {
 //        if(nums==null || nums.length==0) {
 //            return 0;
