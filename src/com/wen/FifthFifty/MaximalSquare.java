ba406c716815c40e1f0fe6131a1bfa75dc67afc3
wen <wenjianxiong91@gmail.com>
2022-03-16 11:01:10 -0400

    Updates.

--- src/com/wen/FifthFifty/MaximalSquare.java
+++ src/com/wen/FifthFifty/MaximalSquare.java
@@ -1,29 +1,49 @@
 package com.wen.FifthFifty;
 
 public class MaximalSquare {
-    // DP
-    // dp[i][j] stands for the edge of the largest square ends at matrix[i][j]
+//    // DP, bottom up
+//    // dp[i][j] stands for the edge of the largest square ends at matrix[i][j]
+//    public int maximalSquare(char[][] matrix) {
+//        if(matrix==null || matrix.length==0 || matrix[0].length==0){
+//            return 0;
+//        }
+//        int m=matrix.length, n=matrix[0].length;
+//        int[][] dp=new int[m][n];
+//        int result=0;
+//        for(int i=0; i<m; i++){
+//            for(int j=0; j<n; j++){
+//                if(matrix[i][j]=='1'){
+//                    if(i==0 || j==0){
+//                        dp[i][j]=1;
+//                    } else{
+//                        // This can be thought of as adding 1 level to the minimum adjacent square
+//                        dp[i][j]=Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]))+1;
+//                    }
+//                }
+//                result=Math.max(result, dp[i][j]);
+//            }
+//        }
+//        return result*result;
+//    }
+
+    // DP, top down
     public int maximalSquare(char[][] matrix) {
-        if(matrix==null || matrix.length==0 || matrix[0].length==0){
-            return 0;
-        }
         int m=matrix.length, n=matrix[0].length;
-        int[][] dp=new int[m][n];
-        int result=0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]=='1'){
-                    if(i==0 || j==0){
-                        dp[i][j]=1;
-                    } else{
-                        // This can be thought of as adding 1 level to the minimum adjacent square
-                        dp[i][j]=Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]))+1;
-                    }
-                }
-                result=Math.max(result, dp[i][j]);
+        int[][] cache=new int[m][n];
+        int[] max=new int[]{0};
+        dp(matrix, cache, m-1, n-1, max);
+        return max[0]*max[0];
+    }
+
+    private int dp(char[][] matrix, int[][] cache, int i, int j, int[] max){
+        if(cache[i][j]==0){
+            int min=Math.min(i>0 && j>0?dp(matrix, cache, i-1, j-1, max):0, Math.min(i>0?dp(matrix, cache, i-1, j, max):0, j>0?dp(matrix, cache, i, j-1, max):0));
+            if(matrix[i][j]=='1'){
+                cache[i][j]=min+1;
+                max[0]=Math.max(max[0], cache[i][j]);
             }
         }
-        return result*result;
+        return cache[i][j];
     }
 
 //    // BFS
