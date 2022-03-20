c2d590565bc6b374454ceaef90ee6dbd24a287b9
wen <wenjianxiong91@gmail.com>
2022-03-20 16:01:35 -0400

    Updates.

--- src/com/wen/FifteenthFifty/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/com/wen/FifteenthFifty/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,22 +1,62 @@
 package com.wen.FifteenthFifty;
 
 public class BestTimeToBuyAndSellStockWithTransactionFee {
-    // buy[i] is the max to buy on day i; sell[i] is the max to sell on day i, which is dependent on maxBuy so far;
-    // pass[i] is the max to do nothing when last action is sell or pass(so buy won't be after another buy)
+//    // DP2
+//    // buy[i] is the max to buy on day i; sell[i] is the max to sell on day i, which is dependent on maxBuy so far;
+//    // pass[i] is the max to do nothing when last action is sell or pass(so buy won't be after another buy)
+//    public int maxProfit(int[] prices, int fee) {
+//        if(prices==null || prices.length<2){
+//            return 0;
+//        }
+//        int l=prices.length;
+//        int[] buy=new int[l], sell=new int[l], pass=new int[l];
+//        buy[0]=-prices[0];
+//        int maxBuy=buy[0];
+//        for(int i=1; i<l; i++){
+//            buy[i]=Math.max(sell[i-1], pass[i-1])-prices[i];
+//            sell[i]=maxBuy+prices[i]-fee;
+//            pass[i]=Math.max(sell[i-1], pass[i-1]);
+//            maxBuy=Math.max(maxBuy, buy[i]);
+//        }
+//        return Math.max(sell[l-1], pass[l-1]);
+//    }
+
+    // DP1, bottom up
     public int maxProfit(int[] prices, int fee) {
-        if(prices==null || prices.length<2){
-            return 0;
-        }
         int l=prices.length;
-        int[] buy=new int[l], sell=new int[l], pass=new int[l];
-        buy[0]=-prices[0];
-        int maxBuy=buy[0];
-        for(int i=1; i<l; i++){
-            buy[i]=Math.max(sell[i-1], pass[i-1])-prices[i];
-            sell[i]=maxBuy+prices[i]-fee;
-            pass[i]=Math.max(sell[i-1], pass[i-1]);
-            maxBuy=Math.max(maxBuy, buy[i]);
+        int[][] dp=new int[l][2];
+        for(int i=l-1; i>=0; i--){
+            if(i==l-1){
+                dp[i][0]=0;
+                dp[i][1]=prices[i]-fee;
+            } else{
+                dp[i][0]=Math.max(dp[i+1][0], dp[i+1][1]-prices[i]);
+                dp[i][1]=Math.max(dp[i+1][1], dp[i+1][0]+prices[i]-fee);
+            }
         }
-        return Math.max(sell[l-1], pass[l-1]);
+        return dp[0][0];
     }
+
+//    // DP1, top down
+//    public int maxProfit(int[] prices, int fee) {
+//        int l=prices.length;
+//        int[][] memo=new int[l][2];
+//        return dp(prices, fee, 0, 0, memo);
+//    }
+//
+//    private int dp(int[] prices, int fee, int day, int hold, int[][] memo){
+//        if(memo[day][hold]==0){
+//            int l=prices.length;
+//            if(day==l-1){
+//                memo[day][hold]=hold==0?0:prices[day]-fee;
+//            } else{
+//                if(hold==0){
+//                    memo[day][hold]=Math.max(dp(prices, fee, day+1, 0, memo), dp(prices, fee, day+1, 1, memo)-prices[day]);
+//                } else{
+//                    memo[day][hold]=Math.max(dp(prices, fee, day+1, 1, memo), dp(prices, fee, day+1, 0, memo)+prices[day]-fee);
+//                }
+//            }
+//        }
+//        return memo[day][hold];
+//    }
 }
--- src/com/wen/NineteenthFifty/MinimumFallingPathSum.java
+++ src/com/wen/NineteenthFifty/MinimumFallingPathSum.java
@@ -1,34 +1,64 @@
 package com.wen.NineteenthFifty;
 
 public class MinimumFallingPathSum {
-    // DPMinimum Falling Path Sum II
-    public int minFallingPathSum(int[][] A) {
-        int m=A.length, n=A[0].length;
-        int[][] dp=new int[m][n];
+//    // DP, bottom up
+//    public int minFallingPathSum(int[][] A) {
+//        int m=A.length, n=A[0].length;
+//        int[][] dp=new int[m][n];
+//        int result=Integer.MAX_VALUE;
+//        for(int i=0; i<m; i++){
+//            for(int j=0; j<n; j++){
+//                if(i==0){
+//                    dp[i][j]=A[i][j];
+//                    if(i==m-1){
+//                        result=Math.min(result, dp[i][j]);
+//                    }
+//                } else{
+//                    int aboveMin=Integer.MAX_VALUE;
+//                    if(j>0){
+//                        aboveMin=Math.min(aboveMin, dp[i-1][j-1]);
+//                    }
+//                    aboveMin=Math.min(aboveMin, dp[i-1][j]);
+//                    if(j<n-1){
+//                        aboveMin=Math.min(aboveMin, dp[i-1][j+1]);
+//                    }
+//                    dp[i][j]=A[i][j]+aboveMin;
+//                    if(i==m-1){
+//                        result=Math.min(result, dp[i][j]);
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
+    // DP, top down
+    public int minFallingPathSum(int[][] matrix) {
+        int m=matrix.length, n=matrix[0].length;
+        int[][] memo=new int[m][n];
         int result=Integer.MAX_VALUE;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i==0){
-                    dp[i][j]=A[i][j];
-                    if(i==m-1){
-                        result=Math.min(result, dp[i][j]);
-                    }
-                } else{
-                    int aboveMin=Integer.MAX_VALUE;
-                    if(j>0){
-                        aboveMin=Math.min(aboveMin, dp[i-1][j-1]);
-                    }
-                    aboveMin=Math.min(aboveMin, dp[i-1][j]);
-                    if(j<n-1){
-                        aboveMin=Math.min(aboveMin, dp[i-1][j+1]);
-                    }
-                    dp[i][j]=A[i][j]+aboveMin;
-                    if(i==m-1){
-                        result=Math.min(result, dp[i][j]);
-                    }
-                }
-            }
+        for(int j=0; j<n; j++){
+            result=Math.min(result, dp(matrix, m-1, j, memo));
         }
         return result;
+    }
+
+    private int dp(int[][] matrix, int i, int j, int[][] memo){
+        if(memo[i][j]==0){
+            if(i==0){
+                memo[i][j]=matrix[i][j];
+            } else{
+                int min=Integer.MAX_VALUE;
+                if(j-1>=0){
+                    min=Math.min(min, dp(matrix, i-1, j-1, memo));
+                }
+                min=Math.min(min, dp(matrix, i-1, j, memo));
+                if(j+1<memo[0].length){
+                    min=Math.min(min, dp(matrix, i-1, j+1, memo));
+                }
+                memo[i][j]=min+matrix[i][j];
+            }
+        }
+        return memo[i][j];
     }
 }
