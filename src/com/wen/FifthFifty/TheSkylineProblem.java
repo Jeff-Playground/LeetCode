8ba932ad5ddf36fd01ea48756a1cebb95b3c6f22
wen <wenjianxiong91@gmail.com>
2022-03-28 12:00:20 -0400

    Updates.

--- src/com/wen/FifthFifty/TheSkylineProblem.java
+++ src/com/wen/FifthFifty/TheSkylineProblem.java
@@ -1,63 +1,50 @@
 package com.wen.FifthFifty;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class TheSkylineProblem {
-    public static List<List<Integer>> getSkyline(int[][] buildings) {
-        if (buildings==null || buildings.length == 0){
-            return new LinkedList<>();
-        } else{
-            return getSkylineDivideAndConquer(buildings, 0, buildings.length-1);
-        }
-    }
-
-    private static LinkedList<List<Integer>> getSkylineDivideAndConquer(int[][] buildings, int start, int end) {
-        if(start<end){
-            int mid=start+(end-start)/2;
-            return merge(getSkylineDivideAndConquer(buildings, start, mid), getSkylineDivideAndConquer(buildings, mid+1, end));
-        } else{
-            LinkedList<List<Integer>> result=new LinkedList<>();
-            result.add(Arrays.asList(buildings[start][0], buildings[start][2]));
-            result.add(Arrays.asList(buildings[start][1], 0));
-            return result;
-        }
-    }
-
-    private static LinkedList<List<Integer>> merge(LinkedList<List<Integer>> l1, LinkedList<List<Integer>> l2) {
-        LinkedList<List<Integer>> result=new LinkedList<>();
-        int h1=0, h2=0;
-        while(!l1.isEmpty() && !l2.isEmpty()){
-            int x=0, h=0;
-            if(l1.getFirst().get(0)<l2.getFirst().get(0)){
-                x=l1.getFirst().get(0);
-                h1=l1.getFirst().get(1);
-                h=Math.max(h1, h2);
-                l1.removeFirst();
-            } else if(l1.getFirst().get(0)>l2.getFirst().get(0)){
-                x=l2.getFirst().get(0);
-                h2=l2.getFirst().get(1);
-                h=Math.max(h1, h2);
-                l2.removeFirst();
-            } else{
-                x=l1.getFirst().get(0);
-                h1=l1.getFirst().get(1);
-                h2=l2.getFirst().get(1);
-                h=Math.max(h1, h2);
-                l1.removeFirst();
-                l2.removeFirst();
-            }
-            if(result.isEmpty() || result.getLast().get(1)!=h){
-                result.add(Arrays.asList(x, h));
-            }
-        }
-        if(!l1.isEmpty()){
-            result.addAll(l1);
-        }
-        if(!l2.isEmpty()){
-            result.addAll(l2);
-        }
-        return result;
-    }
+//    // Line sweep, and add tall first remove short first
+//    public List<List<Integer>> getSkyline(int[][] buildings) {
+//        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b)->a[0]!=b[0]?a[0]-b[0]:(a[2]!=b[2]?b[2]-a[2]:b[1]-a[1]));
+//        TreeMap<Integer, Integer> cache=new TreeMap<>();
+//        for(int[] b: buildings){
+//            pq.offer(new int[]{b[0], b[2], 1});
+//            pq.offer(new int[]{b[1], b[2], -1});
+//        }
+//        List<List<Integer>> result=new ArrayList<>();
+//        int last=0;
+//        while(!pq.isEmpty()){
+//            int[] cur=pq.poll();
+//            if(cur[2]==1){
+//                int x=cur[0], h=cur[1];
+//                cache.put(h, cache.getOrDefault(h, 0)+1);
+//                if(h>last){
+//                    result.add(Arrays.asList(x, h));
+//                    last=h;
+//                }
+//            } else{
+//                // Since the ordering in the PriorityQueue is tall first, use another Deque here to reverse process any
+//                // points at the same location
+//                Deque<int[]> remove=new LinkedList<>();
+//                remove.offer(cur);
+//                while(!pq.isEmpty() && pq.peek()[0]==cur[0]){
+//                    remove.offerLast(pq.poll());
+//                }
+//                while(!remove.isEmpty()){
+//                    cur=remove.pollLast();
+//                    int x=cur[0], h=cur[1];
+//                    if(cache.get(h)==1){
+//                        cache.remove(h);
+//                        if(h==last){
+//                            last=cache.isEmpty()?0:cache.lastKey();
+//                            result.add(Arrays.asList(x, last));
+//                        }
+//                    } else{
+//                        cache.put(h, cache.get(h)-1);
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+//    }
 }
