7a033d141030e4280c3711da9134a2eecef3d94d
wen <wenjianxiong91@gmail.com>
2022-04-05 13:09:30 -0400

    Updates.

--- src/com/wen/SeventeenthFifty/MakingALargeIsland.java
+++ src/com/wen/SeventeenthFifty/MakingALargeIsland.java
@@ -1,26 +1,77 @@
 package com.wen.SeventeenthFifty;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MakingALargeIsland {
-    // Union find, also solvable by DFS and BFS(start with each 0, make it 1 and get the island size)
+//    // Naive BFS, will TLE in JAVA, also solvable by DFS(start with each 0, make it 1 and get the island size)
+//    public int largestIsland(int[][] grid) {
+//        int m=grid.length, n=grid[0].length;
+//        int max=1;
+//        int[][] dirs=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+//        boolean noWater=true;
+//        for(int i=0; i<m; i++){
+//            for(int j=0; j<n; j++){
+//                if(grid[i][j]==0){
+//                    noWater=false;
+//                    grid[i][j]=1;
+//                    Set<Integer> visited=new HashSet<>();
+//                    int total=1;
+//                    visited.add(i*n+j);
+//                    Queue<Integer> q=new LinkedList<>();
+//                    q.offer(i*n+j);
+//                    while(!q.isEmpty()){
+//                        int cur=q.poll();
+//                        int x=cur/n, y=cur%n;
+//                        for(int[] d: dirs){
+//                            int nx=x+d[0], ny=y+d[1];
+//                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1 && !visited.contains(nx*n+ny)){
+//                                visited.add(nx*n+ny);
+//                                q.offer(nx*n+ny);
+//                                total++;
+//                            }
+//                        }
+//                    }
+//                    max=Math.max(max, total);
+//                    grid[i][j]=0;
+//                }
+//            }
+//        }
+//        return noWater?m*n:max;
+//    }
+
+    // Union find
     public int largestIsland(int[][] grid) {
-        if(grid==null || grid.length==0 || grid[0].length==0){
-            throw new IllegalArgumentException("Invalid input!");
-        }
         int m=grid.length, n=grid[0].length;
-        int[][] group=new int[m][n], count=new int[m][n];
-        int islandCount=0, max=Integer.MIN_VALUE;
         int[][] dirs=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        Map<Integer, Integer> root=new HashMap<>();
+        Map<Integer, Integer> count=new HashMap<>();
+        int islandCount=0, max=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1){
-                    int[] total=new int[]{0};
-                    findIsland(grid, i, j, i*n+j, dirs, total, group);
-                    count[i][j]=total[0];
                     islandCount++;
-                    max=Math.max(max, total[0]);
+                    int r=i*n+j;
+                    int total=0;
+                    Queue<Integer> q=new LinkedList<>();
+                    q.offer(r);
+                    grid[i][j]=-1;
+                    total++;
+                    root.put(r, r);
+                    while(!q.isEmpty()){
+                        int cur=q.poll();
+                        int x=cur/n, y=cur%n;
+                        for(int[] d:dirs){
+                            int nx=x+d[0], ny=y+d[1];
+                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1){
+                                grid[nx][ny]=-1;
+                                total++;
+                                q.offer(nx*n+ny);
+                                root.put(nx*n+ny, r);
+                            }
+                        }
+                    }
+                    count.put(r, total);
+                    max=Math.max(max, total);
                 }
             }
         }
@@ -29,52 +80,31 @@ public class MakingALargeIsland {
         } else if(islandCount==1){
             return max<m*n?max+1:max;
         } else{
-            int result=Integer.MIN_VALUE;
+            Set<String> combined=new HashSet<>();
             for(int i=0; i<m; i++){
                 for(int j=0; j<n; j++){
-                    Set<Integer> roots=new HashSet<>();
-                    for(int[] dir: dirs){
-                        int x=i+dir[0], y=j+dir[1];
-                        if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==-1){
-                            int root=findRoot(group, x, y);
-                            roots.add(root);
-                        }
-                        if(roots.size()==0){
-                            continue;
-                        } else{
-                            int cur=0;
-                            for(int r: roots){
-                                cur+=count[r/n][r%n];
+                    if(grid[i][j]==0){
+                        TreeSet<Integer> combine=new TreeSet<>();
+                        int total=1;
+                        for(int[] d: dirs){
+                            int x=i+d[0], y=j+d[1];
+                            if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==-1){
+                                combine.add(root.get(x*n+y));
                             }
-                            result=Math.max(result, cur+1);
+                        }
+                        StringBuilder sb=new StringBuilder();
+                        for(int c: combine){
+                            sb.append(c+",");
+                            total+=count.get(c);
+                        }
+                        if(!combined.contains(sb.toString())){
+                            combined.add(sb.toString());
+                            max=Math.max(max, total);
                         }
                     }
                 }
             }
-            return result;
-        }
-    }
-
-    private int findRoot(int[][] group, int x, int y){
-        int n=group[0].length;
-        while(group[x][y]!=x*n+y){
-            int r=group[x][y];
-            x=r/n;
-            y=r%n;
-        }
-        return group[x][y];
-    }
-
-    private void findIsland(int[][] grid, int i, int j, int root, int[][] dirs, int[] total, int[][] group){
-        int m=grid.length, n=grid[0].length;
-        grid[i][j]=-1;
-        group[i][j]=root;
-        total[0]++;
-        for(int[] dir: dirs){
-            int x=i+dir[0], y=j+dir[1];
-            if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1){
-                findIsland(grid, x, y, root, dirs, total, group);
-            }
+            return max;
         }
     }
 }
