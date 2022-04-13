0f0bf47db6875fdab1706d983e58b501ddb88796
wen <wenjianxiong91@gmail.com>
2022-04-13 19:54:19 -0400

    Updates.

--- src/com/wen/SixthFifty/LQClosestBinarySearchTreeValue.java
+++ src/com/wen/SixthFifty/LQClosestBinarySearchTreeValue.java
@@ -1,5 +1,26 @@
 package com.wen.SixthFifty;
 
+/*
+Given the root of a binary search tree and a target value, return the value in the BST that is closest to the target.
+
+
+
+Example 1:
+Input: root = [4,2,5,1,3], target = 3.714286
+Output: 4
+
+
+Example 2:
+Input: root = [1], target = 4.428571
+Output: 1
+
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+0 <= Node.val <= 109
+-109 <= target <= 109
+ */
+
 public class LQClosestBinarySearchTreeValue {
     public class TreeNode {
         int val;
@@ -8,6 +29,7 @@ public class LQClosestBinarySearchTreeValue {
         TreeNode(int x) { val = x; }
     }
 
+    // Iterative
     // Note here can't simply obtain a min and a max to solve, a BST can be:(note the position of 5 and 8)
     //                5
     //                / \
@@ -28,4 +50,17 @@ public class LQClosestBinarySearchTreeValue {
         }
         return result;
     }
+
+//    // Recursive
+//    public int closestValue(TreeNode root, double target) {
+//        int result=root.val;
+//        TreeNode next=root.val>target?root.left:root.right;
+//        if(next!=null){
+//            int nextResult=closestValue(next, target);
+//            if(Math.abs(result-target)>Math.abs(nextResult-target)){
+//                result=nextResult;
+//            }
+//        }
+//        return result;
+//    }
 }
