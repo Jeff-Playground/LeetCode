728d9778088ff1f5542c2f84263b38d7f652eee6
wen <wenjianxiong91@gmail.com>
2022-04-13 20:30:07 -0400

    Updates.

--- src/com/wen/ThirteenthFifty/ExclusiveTimeOfFunctions.java
+++ src/com/wen/ThirteenthFifty/ExclusiveTimeOfFunctions.java
@@ -4,29 +4,27 @@ import java.util.List;
 import java.util.Stack;
 
 public class ExclusiveTimeOfFunctions {
-    public static int[] exclusiveTime(int n, List<String> logs) {
+    public int[] exclusiveTime(int n, List<String> logs) {
         int[] result=new int[n];
         Stack<Integer> stack=new Stack<>();
-        int lts=-1;
-        for(String log: logs){
-            String[] splited=log.split(":");
+        int lastTs=-1;
+        for(String s: logs){
+            String[] splited=s.split(":");
             int id=Integer.valueOf(splited[0]);
-            String state=splited[1];
-            int ts=Integer.valueOf(splited[2]);
-            if(state.equals("start")){
+            String status=splited[1];
+            // Skew the end ts so both start and end are pointing to the start of a frame
+            int ts=Integer.valueOf(splited[2])+(status.equals("start")?0:1);
+            if(status.equals("start")){
                 if(!stack.isEmpty()){
-                    int last=stack.peek();
-                    if(lts<ts){
-                        result[last]+=ts-lts;
-                    }
+                    int lastId=stack.peek();
+                    result[lastId]+=ts-lastTs;
                 }
                 stack.push(id);
             } else{
                 stack.pop();
-                result[id]+=1;
-                result[id]+=ts-lts+1;
+                result[id]+=ts-lastTs;
             }
-            lts=ts+1;
+            lastTs=ts;
         }
         return result;
     }
