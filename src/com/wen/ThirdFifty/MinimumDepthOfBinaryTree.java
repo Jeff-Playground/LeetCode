59695de4d096ab0c090911b0e71c43b179575dbb
wen <wenjianxiong91@gmail.com>
2022-04-24 10:51:55 -0400

    Updates.

--- src/com/wen/ThirdFifty/MinimumDepthOfBinaryTree.java
+++ src/com/wen/ThirdFifty/MinimumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package com.wen.ThirdFifty;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MinimumDepthOfBinaryTree {
     public static class TreeNode {
         int val;
@@ -8,19 +11,48 @@ public class MinimumDepthOfBinaryTree {
         public TreeNode(int x) { val = x; }
     }
 
+//    // Tree post order traversal
+//    public int minDepth(TreeNode root) {
+//        if(root==null){
+//            return 0;
+//        }
+//        if(root.left==null && root.right==null){
+//            return 1;
+//        } else if(root.left==null){
+//            return 1+minDepth(root.right);
+//        } else if(root.right==null){
+//            return 1+minDepth(root.left);
+//        } else{
+//            return 1+Math.min(minDepth(root.left), minDepth(root.right));
+//        }
+//    }
+
+    // Tree level order traversal
     public int minDepth(TreeNode root) {
         if(root==null){
             return 0;
-        }
-        if(root.left==null && root.right==null){
-            return 1;
-        }
-        if(root.left==null){
-            return 1+minDepth(root.right);
-        } else if(root.right==null){
-            return 1+minDepth(root.left);
         } else{
-            return 1+Math.min(minDepth(root.left), minDepth(root.right));
+            Queue<TreeNode> q=new LinkedList<>();
+            q.offer(root);
+            int result=1;
+            while(!q.isEmpty()){
+                int size=q.size();
+                while(size-->0){
+                    TreeNode cur=q.poll();
+                    if(cur.left==null && cur.right==null){
+                        return result;
+                    } else{
+                        if(cur.left!=null){
+                            q.offer(cur.left);
+                        }
+                        if(cur.right!=null){
+                            q.offer(cur.right);
+                        }
+                    }
+                }
+                result++;
+            }
+            return result;
         }
     }
 }
