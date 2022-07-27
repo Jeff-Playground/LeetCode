d89fc8eed34e241c5b21edd641ac8bc749057845
wen <wenjianxiong91@gmail.com>
2022-07-27 18:28:34 -0400

    Updates.

--- src/com/wen/TwentySeventhFifty/MaximumStudentsTakingExam.java
+++ src/com/wen/TwentySeventhFifty/MaximumStudentsTakingExam.java
@@ -3,7 +3,7 @@ package com.wen.TwentySeventhFifty;
 import java.util.Arrays;
 
 public class MaximumStudentsTakingExam {
-    // Hungarian algorithm
+    // Hungarian algorithm, DFS
     // First convert the given seats into a graph, note the graph only contains edges from nodes on even columns to nodes
     // on odd columns, this is to ensure the graph is bipartite.
     // Then find the maximum match in the bipartite graph, in each match there will be 1 seat selected, and for the unmatched
@@ -11,6 +11,7 @@ public class MaximumStudentsTakingExam {
     public int maxStudents(char[][] seats) {
         int m=seats.length, n=seats[0].length;
         int[][] graph=new int[m*n][m*n];
+        // Note these deltas are used to find edges for the graph
         int[][] dirs=new int[][]{{0,-1}, {-1,-1}, {1,-1}, {0,1}, {1,1}, {-1,1}};
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j+=2){
