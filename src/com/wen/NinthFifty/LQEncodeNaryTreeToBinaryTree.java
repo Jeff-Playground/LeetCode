13a73d030e806ad97b2aac0d8129f396614105bc
wen <wenjianxiong91@gmail.com>
2022-10-20 17:19:16 -0400

    Updates.

--- src/com/wen/NinthFifty/LQEncodeNaryTreeToBinaryTree.java
+++ src/com/wen/NinthFifty/LQEncodeNaryTreeToBinaryTree.java
@@ -4,88 +4,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LQEncodeNaryTreeToBinaryTree {
-    // Definition for Undirected graph.
-    class UndirectedGraphNode {
-        int label;
-        List<UndirectedGraphNode> neighbors;
-        UndirectedGraphNode(int x) {
-            label = x;
-            neighbors = new ArrayList<UndirectedGraphNode>();
-        }
-    }
-    // Definition of TreeNode:
-    public class TreeNode {
+    class Node {
         public int val;
-        public TreeNode left, right;
-        public TreeNode(int val) {
-            this.val = val;
-            this.left = this.right = null;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
         }
     }
 
-    /**
-     * @param root: binary tree
-     * @return: N-ary tree
-     */
-    public UndirectedGraphNode decode(TreeNode root) {
-        if(root==null){
-            return null;
-        }
-        UndirectedGraphNode result=new UndirectedGraphNode(root.val);
-        decode(root, result, true);
-        return result;
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
     }
 
-    private void decode(TreeNode curTree, UndirectedGraphNode cur, boolean childInLeft){
-        if(childInLeft){
-            if(curTree.left!=null){
-                TreeNode node=curTree.left;
-                while(node!=null){
-                    UndirectedGraphNode e=new UndirectedGraphNode(node.val);
-                    decode(node, e, !childInLeft);
-                    cur.neighbors.add(e);
-                    node=node.left;
+    class Codec {
+        // Encodes an n-ary tree to a binary tree.
+        public TreeNode encode(Node root) {
+            return encodeHelper(root, 0);
+        }
+
+        private TreeNode encodeHelper(Node cur, int level){
+            if(cur==null){
+                return null;
+            } else{
+                TreeNode root=new TreeNode(cur.val);
+                TreeNode t=root;
+                if(cur.children!=null){
+                    for(Node e: cur.children){
+                        TreeNode et=encodeHelper(e,level+1);
+                        if(level%2==0){
+                            t.left=et;
+                        } else{
+                            t.right=et;
+                        }
+                        t=et;
+                    }
                 }
-            }
-        } else{
-            if(curTree.right!=null){
-                TreeNode node=curTree.right;
-                while(node!=null){
-                    UndirectedGraphNode e=new UndirectedGraphNode(node.val);
-                    decode(node, e, !childInLeft);
-                    cur.neighbors.add(e);
-                    node=node.right;
-                }
+                return root;
             }
         }
-    }
 
-    /**
-     * @param root: N-ary tree
-     * @return: binary tree
-     */
-    public TreeNode encode(UndirectedGraphNode root) {
-        if(root==null){
-            return null;
+        // Decodes your binary tree to an n-ary tree.
+        public Node decode(TreeNode root) {
+            return decodeHelper(root, 0);
         }
-        TreeNode result=new TreeNode(root.label);
-        encode(root, result, true);
-        return result;
-    }
 
-    private void encode(UndirectedGraphNode cur, TreeNode curTree, boolean childInLeft){
-        List<UndirectedGraphNode> children=cur.neighbors;
-        if(children.size()>0){
-            TreeNode node=curTree;
-            for(UndirectedGraphNode e: children){
-                TreeNode eTree=new TreeNode(e.label);
-                encode(e, eTree, !childInLeft);
-                if(childInLeft){
-                    node.left=eTree;
+        private Node decodeHelper(TreeNode cur, int level){
+            if(cur==null){
+                return null;
+            } else{
+                Node root=new Node(cur.val);
+                root.children=new ArrayList<>();
+                TreeNode t=cur;
+                if(level%2==0){
+                    t=t.left;
+                    while(t!=null){
+                        root.children.add(decodeHelper(t,level+1));
+                        t=t.left;
+                    }
                 } else{
-                    node.right=eTree;
+                    t=t.right;
+                    while(t!=null){
+                        root.children.add(decodeHelper(t,level+1));
+                        t=t.right;
+                    }
                 }
-                node=eTree;
+                return root;
             }
         }
     }
