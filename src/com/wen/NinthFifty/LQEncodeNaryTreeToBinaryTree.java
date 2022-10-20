1b943aa1909af4c1dd91455d6376b4258e4a7f6f
wen <wenjianxiong91@gmail.com>
2022-10-20 17:45:17 -0400

    Updates.

--- src/com/wen/NinthFifty/LQEncodeNaryTreeToBinaryTree.java
+++ src/com/wen/NinthFifty/LQEncodeNaryTreeToBinaryTree.java
@@ -27,60 +27,100 @@ public class LQEncodeNaryTreeToBinaryTree {
         TreeNode(int x) { val = x; }
     }
 
+    // Encode by putting all children in right node, note the siblings are put as left nodes of the right node, this
+    // way there's no need to passing down the level info
     class Codec {
         // Encodes an n-ary tree to a binary tree.
         public TreeNode encode(Node root) {
-            return encodeHelper(root, 0);
-        }
-
-        private TreeNode encodeHelper(Node cur, int level){
-            if(cur==null){
+            if(root==null){
                 return null;
             } else{
-                TreeNode root=new TreeNode(cur.val);
-                TreeNode t=root;
-                if(cur.children!=null){
-                    for(Node e: cur.children){
-                        TreeNode et=encodeHelper(e,level+1);
-                        if(level%2==0){
-                            t.left=et;
-                        } else{
-                            t.right=et;
-                        }
-                        t=et;
+                TreeNode r=new TreeNode(root.val);
+                if(root.children!=null && root.children.size()>0){
+                    TreeNode t=encode(root.children.get(0));
+                    r.right=t;
+                    for(int i=1; i<root.children.size(); i++){
+                        t.left=encode(root.children.get(i));
+                        t=t.left;
                     }
                 }
-                return root;
+                return r;
             }
         }
 
         // Decodes your binary tree to an n-ary tree.
         public Node decode(TreeNode root) {
-            return decodeHelper(root, 0);
-        }
-
-        private Node decodeHelper(TreeNode cur, int level){
-            if(cur==null){
+            if(root==null){
                 return null;
             } else{
-                Node root=new Node(cur.val);
-                root.children=new ArrayList<>();
-                TreeNode t=cur;
-                if(level%2==0){
-                    t=t.left;
-                    while(t!=null){
-                        root.children.add(decodeHelper(t,level+1));
-                        t=t.left;
-                    }
-                } else{
-                    t=t.right;
-                    while(t!=null){
-                        root.children.add(decodeHelper(t,level+1));
-                        t=t.right;
-                    }
+                Node r=new Node(root.val);
+                r.children=new ArrayList<>();
+                TreeNode tmp=root.right;
+                while(tmp!=null){
+                    r.children.add(decode(tmp));
+                    tmp=tmp.left;
                 }
-                return root;
+                return r;
             }
         }
     }
+
+//    // Put the children in either left or right subtree based on the level of the root node, this would require
+//    // passing down the level as an input
+//    class Codec {
+//        // Encodes an n-ary tree to a binary tree.
+//        public TreeNode encode(Node root) {
+//            return encodeHelper(root, 0);
+//        }
+//
+//        private TreeNode encodeHelper(Node cur, int level){
+//            if(cur==null){
+//                return null;
+//            } else{
+//                TreeNode root=new TreeNode(cur.val);
+//                TreeNode t=root;
+//                if(cur.children!=null){
+//                    for(Node e: cur.children){
+//                        TreeNode et=encodeHelper(e,level+1);
+//                        if(level%2==0){
+//                            t.left=et;
+//                        } else{
+//                            t.right=et;
+//                        }
+//                        t=et;
+//                    }
+//                }
+//                return root;
+//            }
+//        }
+//
+//        // Decodes your binary tree to an n-ary tree.
+//        public Node decode(TreeNode root) {
+//            return decodeHelper(root, 0);
+//        }
+//
+//        private Node decodeHelper(TreeNode cur, int level){
+//            if(cur==null){
+//                return null;
+//            } else{
+//                Node root=new Node(cur.val);
+//                root.children=new ArrayList<>();
+//                TreeNode t=cur;
+//                if(level%2==0){
+//                    t=t.left;
+//                    while(t!=null){
+//                        root.children.add(decodeHelper(t,level+1));
+//                        t=t.left;
+//                    }
+//                } else{
+//                    t=t.right;
+//                    while(t!=null){
+//                        root.children.add(decodeHelper(t,level+1));
+//                        t=t.right;
+//                    }
+//                }
+//                return root;
+//            }
+//        }
+//    }
 }
