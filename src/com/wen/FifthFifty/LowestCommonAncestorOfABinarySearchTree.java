73b06fc2b53970118fe30087342d464a2922574e
wen <wenjianxiong91@gmail.com>
2022-11-08 17:30:49 -0500

    Updates.

--- src/com/wen/FifthFifty/LowestCommonAncestorOfABinarySearchTree.java
+++ src/com/wen/FifthFifty/LowestCommonAncestorOfABinarySearchTree.java
@@ -8,16 +8,29 @@ public class LowestCommonAncestorOfABinarySearchTree {
         TreeNode(int x) { val = x; }
     }
 
+//    // Iterative
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        int max=Math.max(p.val, q.val);
+//        int min=Math.min(p.val, q.val);
+//        while(root.val<min || root.val>max){
+//            if(root.val<min){
+//                root=root.right;
+//            } else{
+//                root=root.left;
+//            }
+//        }
+//        return root;
+//    }
+
+    // Recursive
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int max=Math.max(p.val, q.val);
-        int min=Math.min(p.val, q.val);
-        while(root.val<min || root.val>max){
-            if(root.val<min){
-                root=root.right;
-            } else{
-                root=root.left;
-            }
+        int minValue=Math.min(p.val, q.val), maxValue=Math.max(p.val, q.val);
+        if(root.val<minValue){
+            return lowestCommonAncestor(root.right, p, q);
+        } else if(root.val>maxValue){
+            return lowestCommonAncestor(root.left, p, q);
+        } else{
+            return root;
         }
-        return root;
     }
 }
